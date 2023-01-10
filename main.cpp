892eae9926e536ba0305abdf1447f8b72c322c88
600883 <89355523+600883@users.noreply.github.com>
2023-01-10 20:18:47 +0100

    Add files via upload
    Min første commit

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include<iostrean>
+#include<string>
+
+using namespace std;
+
+int main() {
+    wchar_t s[] = L"Its a nice day, my house is on fire";
+    int n = 0;
+    for(wchar_t *p = 0; *p != 0; *p++) {
+        if(*p == 'e') {
+            n++;
+    }
+    wcout << L"Number of e: " << n << endl;
+    return 0;
+}
